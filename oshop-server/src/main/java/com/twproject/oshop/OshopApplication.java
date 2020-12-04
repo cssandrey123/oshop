b92beebb254a703ce8c1c8bbe35972c852baff01
Mara Sferdian <marasferdian@gmail.com>
2020-12-05 00:01:13 +0200

    Changed project structure - separated in server and client side

---
+++ oshop-server/src/main/java/com/twproject/oshop/OshopApplication.java
@@ -0,0 +1,13 @@
+package com.twproject.oshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OshopApplication.class, args);
+	}
+
+}
---
+++ oshop-server/src/test/java/com/twproject/oshop/OshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twproject.oshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
