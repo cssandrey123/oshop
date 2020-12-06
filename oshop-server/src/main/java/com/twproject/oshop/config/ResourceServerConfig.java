d701ab6380345ed3636982962652311137cd56b5
Mara Sferdian <marasferdian@gmail.com>
2020-12-06 11:38:25 +0200

    Backend to backend communication with Google :)

--- oshop-server/src/main/java/com/twproject/oshop/config/ResourceServerConfig.java
+++ oshop-server/src/main/java/com/twproject/oshop/config/ResourceServerConfig.java
@@ -1,20 +1,43 @@
 package com.twproject.oshop.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.twproject.oshop.model.User;
+import com.twproject.oshop.persistence.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.http.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
 
 @Configuration
 @EnableResourceServer
 @Slf4j
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    @Autowired
+   private UserRepository userRepository;
+    @Autowired
+    private OAuth2AuthorizedClientService authorizedClientService;
     @Value("oshop")
     private String resourceId;
+
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) {
@@ -24,12 +47,39 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/login").permitAll()
-                .antMatchers("/login-oshop").permitAll()
-                .antMatchers(HttpMethod.GET, "**").permitAll()
-                .antMatchers(HttpMethod.OPTIONS, "**").permitAll()
-                .anyRequest().authenticated()
+                    .antMatchers("/login").permitAll()
+                    .antMatchers("/login-oshop").permitAll()
+                // .antMatchers(HttpMethod.GET, "**").permitAll()
+                    .antMatchers(HttpMethod.OPTIONS, "**").permitAll()
+                    .anyRequest().authenticated()
                 .and()
-                .oauth2Login();
+                    .oauth2Login().successHandler(this::successHandler);
     }
+
+    private void successHandler(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) {
+        OAuth2AuthenticationToken oauthToken =
+                (OAuth2AuthenticationToken) authentication;
+        OAuth2AuthorizedClient client = authorizedClientService.loadAuthorizedClient(oauthToken.getAuthorizedClientRegistrationId(), oauthToken.getName());
+        String userInfoEndpointUri = client.getClientRegistration()
+                .getProviderDetails().getUserInfoEndpoint().getUri();
+
+        if (!StringUtils.isEmpty(userInfoEndpointUri)) {
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + client.getAccessToken()
+                    .getTokenValue());
+            HttpEntity entity = new HttpEntity("", headers);
+            ResponseEntity<Map> response = restTemplate
+                    .exchange(userInfoEndpointUri, HttpMethod.GET, entity, Map.class);
+            Map userAttributes = response.getBody();
+            User googleUser = new User();
+            googleUser.setRole("DEFAULT");
+            googleUser.setEmail((String) userAttributes.getOrDefault("email", null));
+            String username = userAttributes.getOrDefault("name",null).toString().replace(" ","").toLowerCase();
+            googleUser.setUsername(username);
+            userRepository.save(googleUser);
+
+        }
+    }
+
 }
