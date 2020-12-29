aaaea99f8864a07c42569eba4ec2bac8c659a8bf
Ispas Andrei <andrei@devplant.ro>
2020-12-29 15:47:12 +0200

    begin refactoring

--- oshop-client/src/app/services/rest.service.ts
+++ oshop-client/src/app/services/rest.service.ts
@@ -1,9 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {environment} from '../../environments/environment';
 import {CookieService} from 'ngx-cookie-service';
 import {Observable, of} from 'rxjs';
-import {mergeMap, tap} from 'rxjs/operators';
+import {tap} from 'rxjs/operators';
 import {User} from "../models/user.model";
 
 @Injectable()
