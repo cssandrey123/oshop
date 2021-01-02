966539200c573f1dd3fa74787d5ad0e6f7144e5e
Mara Sferdian <marasferdian@gmail.com>
2021-01-02 17:55:06 +0200

    Fixed problem with navbar username after login

--- oshop-client/src/app/components/bg-navbar/bg-navbar.component.ts
+++ oshop-client/src/app/components/bg-navbar/bg-navbar.component.ts
@@ -3,33 +3,32 @@ import {AfterViewInit, Component, OnInit} from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { AppUser } from 'src/app/models/app-user';
 import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
-import {Observable, of} from 'rxjs';
+import {Observable, of, timer} from 'rxjs';
 import { ShoppingCart } from 'src/app/models/shopping-cart';
 import {RestService} from "../../services/rest.service";
 import {User} from "../../models/user.model";
 import {UserService} from "../../services/user.service";
-import {map, switchMap, tap} from "rxjs/operators";
+import {map, mergeMap, switchMap, tap} from "rxjs/operators";
 
 @Component({
   selector: 'app-bg-navbar',
   templateUrl: './bg-navbar.component.html',
   styleUrls: ['./bg-navbar.component.css']
 })
-export class BgNavbarComponent implements OnInit, AfterViewInit {
+export class BgNavbarComponent implements OnInit {
   public isMenuCollapsed = true;
   cart$: Observable<ShoppingCart>;
   currentUser: User;
-  user$: Observable<User>;
+  user$: Observable<User> = this.userService.user$;
   constructor(private auth: AuthService, private shoppingCartService: ShoppingCartService, private restService: RestService, private userService: UserService) { }
 
 
   ngOnInit()  {
     // Displaying the numbers of products from shopping cart in navbar
     this.cart$ = this.shoppingCartService.getCart();
+    this.user$.subscribe(user => this.currentUser = user);
   }
-  ngAfterViewInit(): void {
-   this.user$ = this.userService.user$;
-  }
+
 
   async isCurrentUserPresent() {
    return this.userService.currentUser !== null;
